e6c1a545efb69c30eac78efb20f258ef9d2285d9
MohammadImrulIbtehaz <106837741+MohammadImrulIbtehaz@users.noreply.github.com>
2023-12-26 04:51:45 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+echo "Hello world";
+
+
+?>
+<br>
+<br>
+<br>
+
+<?php
+$names = "Imrul Ibtehaz Orvee";
+
+echo "My name is ". $names . "." ;
+
+?>
+<br>
+<br>
+<br>
+<?php
+$a ="15";
+$b ="36";
+
+echo "$a <br>";
+echo $b;
+?>
+<br>
+<br>
+<br>
+<?php
+$x = 22;
+$y = 24;
+echo $x + $y;
+?>
+<br>
+<br>
+<br>
+<?php
+$x = 20;
+var_dump($x);
+?>
+<br>
+<br>
+<br>
+<?php
+$name ="Orvee";
+$age = 25;
+var_dump($name);
+var_dump($age);
+?>
+<br>
+<br>
+<?php
+$p = 22;
+var_dump ($p/3);
+?>
